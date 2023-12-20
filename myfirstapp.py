8e2de5354bb2f811b1ced01896b8ce3ea4761708
NoorAqilah <154412276+NoorAqilah@users.noreply.github.com>
2023-12-20 16:41:40 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+
 st.header("My first Streamlit App")
 
 option = st.sidebar.selectbox(
@@ -25,4 +26,16 @@ elif option=='T n C':
         st.write(pd.DataFrame({
         'Students': ['John', 'Lofa', 'Siti', 'Amy'],
         'Attendance Status': ['yes', 'yes', 'yes', 'no']
-}))
+    }))
+elif option=='Long Process':
+    'Starting a long computation...'
+
+    latest_iteration = st.empty()
+    bar = st.progress(0)
+
+    for i in range(100):
+        latest_iteration.text(f'Iteration {i+1}')
+        bar.progress(i + 1)
+        time.sleep(0.1)
+
+    '...and now we\'re done!'
